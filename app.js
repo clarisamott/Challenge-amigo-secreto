316a3375c0efd5d2f5f1ccaa16b11f0dd43e4e81
Clarisa Mottalini~ <clarisamott@gmail.com>
2025-08-01 23:48:03 -0300

    Actualizo cambios

--- app.js
+++ app.js
@@ -7,15 +7,24 @@ boton.addEventListener ("click", agregarAmigo());
 */
 
 function limpiarCaja(){
-    document.querySelector("#amigo").value = " ";
+    document.querySelector("#amigo").value = "";
 }
 
 function agregarAmigo(){
-    if (nombresUsuario = ""){
+    if (nombresUsuario.value === ""){
         alert("Por favor, inserte un nombre");
     } else {
         listaNombres.push(nombresUsuario);
         limpiarCaja();
+        console.log(listaNombres);
     }
+}
+
+
+function mostrarNombres(){
+    for (let i=0; i<listaNombres.length; i++);
+    console.log (listaNombres[i]);
+    listaNombres[i]= document.createElement("li");
+
 }
 
